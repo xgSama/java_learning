a3fb6b2d4792817bf205cc75531eb2f7bfc9acc3
xgSama <china_cyzyc@163.com>
2021-03-10 20:02:26 +0800

    leetcode 12、19

---
+++ algorithm/src/main/java/com/xgsama/algorithm/leetcode/middle/_12_IntegerToRoman.java
@@ -0,0 +1,35 @@
+package com.xgsama.algorithm.leetcode.middle;
+
+/**
+ * _12_IntegerToRoman
+ *
+ * @author xgSama
+ * @date 2021/3/10 16:59
+ */
+public class _12_IntegerToRoman {
+
+    // 计算出六种特殊的情况，并添加到符号池中，此时共有12种符号可用的，且在罗马数字中从左往右一定是是递减的
+    private static final int[] values = {   1000, 900,  500,  400, 100, 90,   50,  40,   10,   9,    5,   4,    1};
+    private static final String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+
+    public static String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        // 在转换过程中，应该使用尽可能大的符号，在符号池中从左往右取值
+        for (int i = 0; i < values.length && num >= 0; i++) {
+            // 减去当前符号后如果此符号依旧满足条件，则应该继续使用该符号
+            while (values[i] <= num) {
+                num -= values[i];
+                sb.append(symbols[i]);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("intToRoman(140) = " + intToRoman(140));
+
+    }
+
+}
---
+++ algorithm/src/main/java/com/xgsama/algorithm/leetcode/middle/_19_RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,82 @@
+package com.xgsama.algorithm.leetcode.middle;
+
+import com.xgsama.algorithm.leetcode.model.ListNode;
+
+import java.util.Stack;
+
+/**
+ * _19_RemoveNthNodeFromEndOfList
+ *
+ * @author xgSama
+ * @date 2021/3/10 10:35
+ */
+public class _19_RemoveNthNodeFromEndOfList {
+
+    /*
+     * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+     */
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+//        ListNode l2 = new ListNode(2);
+//        l1.next = l2;
+//        ListNode l3 = new ListNode(3);
+//        l2.next = l3;
+//        ListNode l4 = new ListNode(4);
+//        l3.next = l4;
+
+        ListNode node = removeNthFromEnd(l1, 2);
+
+        while (node != null) {
+            System.out.print(l1.val + " ");
+            node = node.next;
+        }
+    }
+
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode dummy = new ListNode(-1, head);
+
+        ListNode fast = head;
+        ListNode slow = dummy;
+
+        // 循环结束后，fast指向第n个节点，slow指向-1节点，两指针间隔n-1个节点
+        for (int i = 0; i < n; i++) {
+            if (fast != null)
+                fast = fast.next;
+            else return dummy.next;
+        }
+        // 当fast处于尾部时，slow指向倒数n+1个节点
+        // fast是倒数第一个节点，与倒数第n个节点之间间隔了n-2个节点，所以此时slow位于倒数第n个节点的前一个节点
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        // 可能删除的恰好是head节点，所以返回dummy.next最合适
+        return dummy.next;
+
+    }
+
+    public static ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1, head);
+        ListNode tmp = dummy;
+        Stack<ListNode> stack = new Stack<>();
+
+        while (tmp != null) {
+            stack.push(tmp);
+            tmp = tmp.next;
+        }
+
+        for (int i = 0; i < n; i++) {
+            stack.pop();
+        }
+
+        ListNode pop = stack.peek();
+        pop.next = pop.next.next;
+
+        return dummy.next;
+    }
+}
